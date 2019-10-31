8a0535070b38576d3e4c1165b234dc98cc8eb2c3
Jelena <stojanovicj@rocketmail.com>
2019-10-31 16:22:19 +0100

    domaci

--- Nedelja2/cetvrtak/zadatak6.js
+++ Nedelja2/cetvrtak/zadatak6.js
@@ -3,9 +3,9 @@ var number = 0;
 for(let i=1; i<=100; i++){
 number = number + i;
       if (i%3 == 0 && i%5 == 0)
-            console.log('FuzzBuzz');
+            console.log('FizzBuzz');
       else if (i%3 == 0 && i%5 != 0)
-            console.log('Fuzz');
+            console.log('Fizz');
       else if (i%3 != 0 && i%5 == 0)
             console.log('Buzz');
       else if (i%3 != 0 && i%5 != 0)

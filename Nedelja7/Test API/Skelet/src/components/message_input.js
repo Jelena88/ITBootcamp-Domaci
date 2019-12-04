934e2ea506aa71aae3eaf028aed052d2022c3170
Jelena <stojanovicj@rocketmail.com>
2019-12-04 20:54:00 +0100

    domaci

--- Nedelja7/Test API/Skelet/src/components/message_input.js
+++ Nedelja7/Test API/Skelet/src/components/message_input.js
@@ -6,7 +6,7 @@ class MessageInput {
 
         this.message = document.createElement('input');
         this.message.type = 'text';
-        this.message.placeholder = 'Poruka'; // prostor za poruku
+        this.message.placeholder = 'Poruka'; 
 
         this.username = document.createElement('input');
         this.username.type = 'text';
@@ -20,16 +20,33 @@ class MessageInput {
         this.sendButton = document.createElement('button');
         this.sendButton.textContent = 'Posalji poruku'; 
 
+        
+        this.sortButton = document.createElement('button');
+        this.sortButton.textContent = 'Sortiraj'; 
 
         
+        this.node.appendChild(this.sortButton);
         this.node.appendChild(this.message);
         this.node.appendChild(this.sendButton); 
-    }
+
+
+        this.set.addEventListener('click', () => {
+            if(this.set.clicked!==true){
+                alert ("potvrdi username")}
+        }
+       
+
+        )}
     getNode() {
         return this.node;
     }
     btn_send_on_click(on_click) {
         this.sendButton.addEventListener('click', on_click); 
+    }
+
+
+    sort(onClick2) {
+        this.set.addEventListener('click', onClick2);
     }
 }
 
--- Nedelja7/Test API/Skelet/src/layout/content.js
+++ Nedelja7/Test API/Skelet/src/layout/content.js
@@ -8,21 +8,25 @@ import { send_message } from "../utilities/ser";
 class Content {
     constructor() {
 
-        let user = ''
+     
         this.node = document.createElement('main');
 
 
         let messages = new Messages();
         let message_input = new MessageInput();
    
+        //setBtnMessageOnClick
+  
 
         message_input.btn_send_on_click(() => {
+          
+
             if(message_input.message.value.trim() == '' || message_input.username.value.trim() =='' )
             return alert('Polje za poruku i username ne moze biti prazno');
             send_message(message_input.username.value, message_input.message.value)
             .then(response => {
                 if(response.success == true) {
-                    messages;
+                    messages.getNode();
                 }
             })
 
